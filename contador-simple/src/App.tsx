62c3d5e623cb84bc61d45d4d499d34070b9741a7
Ignacio Cambra Alegre <icambraa@uoc.edu>
2024-10-03 22:41:06 +0200

    counter done

--- contador-simple/src/App.tsx
+++ contador-simple/src/App.tsx
@@ -1,14 +1,45 @@
-
+import React, { useEffect, useState } from "react";
 
 function App() {
 
 
+  const [numero, setNumero] = useState(0);
+  const [mostrar, setMostrar] = useState(false);
+
+  useEffect(() => {
+    const buttons = document.querySelectorAll("button");
+    buttons.forEach(button => {
+      button.style.backgroundColor = `#${Math.floor(Math.random()*16777215).toString(16)}`;
+    });
+  }, [numero]);
+
+  const incrementar = () => {
+    setNumero(numero + 1);
+  }
+
+  const decrementar = () => {
+    setNumero(numero - 1);
+  }
+
   return (
     <>
-      <h1 className="font-bold">Hola a todos</h1>
-      <div>Hola</div>
+      <div className="flex h-screen items-center justify-center">
+        <div className="flex flex-col gap-6">
+          <div className="flex items-center justify-center">
+            <h1 className=" flex text-4xl">{numero}</h1>
+          </div>
+          <div className="flex flex-col gap-2">
+            <button onClick = {incrementar} className="flex bg-slate-800 text-white p-2 rounded-lg font-bold">
+              Incrementar
+            </button>
+            <button onClick={decrementar} className="flex bg-slate-800 text-white p-2 rounded-lg font-bold">
+              Decrementar
+            </button>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
