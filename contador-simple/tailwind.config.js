b9787ec776e5f0295191a9406c61d81eb9bad0e5
Ignacio Cambra Alegre <icambraa@uoc.edu>
2024-10-03 22:19:36 +0200

    First commit

---
+++ contador-simple/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Aquí especificamos los archivos que Tailwind debe escanear
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
